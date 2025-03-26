045d29607f4009ddee0197c7dfc0e5a433ce478c
Eduardo Cortés Herrera <eduardocortes3373@gmail.com>
2025-03-26 15:11:16 -0600

    Create base configurations

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import 'express-async-errors';
+
+export const app = express();
+
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: err.message || 'An error occurred while processing the request' });
+});
