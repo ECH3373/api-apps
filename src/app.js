d837718086e3ba0ee807e726df7fc45cbfefd6e5
Eduardo Cortés Herrera <eduardocortes3373@gmail.com>
2025-03-26 16:08:25 -0600

    Create app feature

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
+import { app as application } from './features/app/index.js';
 
 export const app = express();
 
 app.use(express.json());
+
+app.use('/api/v1/apps', application.router);
 
 app.use((err, req, res, next) => {
   res.status(500).json({ error: err.message || 'An error occurred while processing the request' });
---
+++ src/features/app/index.js
@@ -0,0 +1,11 @@
+import { controller } from './app.controller.js';
+import { model } from './app.model.js';
+import { query } from './app.query.js';
+import { router } from './app.router.js';
+
+export const app = {
+  controller,
+  model,
+  query,
+  router,
+};
