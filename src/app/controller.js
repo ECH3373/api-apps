b7356a310080eb612ad681c90fb91198d82c3dd9
Eduardo Cortés Herrera <eduardocortes3373@gmail.com>
2025-04-01 15:17:02 -0600

    Fix app conntroller

--- src/app/controller.js
+++ src/app/controller.js
@@ -1,12 +1,12 @@
 import { services } from '../shared/services/index.js';
 
 const index = async (req, res) => {
-  const { data, meta } = await services.crud.index({ model: 'apps', query: req.query, search: ['name'] });
+  const { data, meta } = await services.crud.index({ model: 'app', query: req.query, search: ['name'] });
   return services.response.send({ res, data, meta, message: 'the list of apps has been successfully retrieved' });
 };
 
 const show = async (req, res) => {
-  const data = await services.crud.show({ model: 'apps', target: req.params.id, keys: ['id'] });
+  const data = await services.crud.show({ model: 'app', target: req.params.id, keys: ['id'] });
   if (!data) return services.response.send({ res, data, error: 'the app with the provided ID does not exist' });
   services.response.send({ res, data, message: 'app successfully retrieved' });
 };
